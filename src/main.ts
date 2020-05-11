c2ad64760f5ad2c00c7ecd1708399a28f33ca698
David Karlsen <david@davidkarlsen.com>
2020-05-12 00:41:04 +0200

    cleanup context

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ const STATE_FLAG = 'cloudctlLogin'
 export async function run(): Promise<void> {
   if (isCleanupPhase()) {
     await exec.exec('cloudctl', ['logout'])
+    if (core.getState('context').length > 0) {
+      await exec.exec('kubectl', [
+        'config',
+        'delete',
+        'context',
+        core.getState('context')
+      ])
+    }
   } else {
     if (core.getInput('installKubectl', {required: true}) === 'true') {
       await downloadTool('kubectl')
@@ -35,17 +43,31 @@ async function downloadTool(tool: string): Promise<void> {
 async function cloudctlLogin(): Promise<void> {
   try {
     await exec
-      .exec('cloudctl', [
-        'login',
-        '-a',
-        core.getInput('apiEndpoint', {required: true}),
-        '-u',
-        core.getInput('username', {required: true}),
-        '-p',
-        core.getInput('password', {required: true}),
-        '-n',
-        core.getInput('namespace', {required: true})
-      ])
+      .exec(
+        'cloudctl',
+        [
+          'login',
+          '-a',
+          core.getInput('apiEndpoint', {required: true}),
+          '-u',
+          core.getInput('username', {required: true}),
+          '-p',
+          core.getInput('password', {required: true}),
+          '-n',
+          core.getInput('namespace', {required: true})
+        ],
+        {
+          listeners: {
+            stdline: (data: string) => {
+              const match = data.match(/Context "(?<context>.*)" created./)
+              if (match?.groups) {
+                const context = match.groups['context']
+                core.saveState('context', context)
+              }
+            }
+          }
+        }
+      )
       .then(() => core.saveState(STATE_FLAG, 'true'))
   } catch (error) {
     core.setFailed(error.message)
